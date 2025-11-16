ef28176a095ff34e79b8395c1a3ad1ae7e270e0d
Giorgi Kobaidze <george.kobaidze.14@gmail.com>
2025-11-16 16:10:35 +0400

    Add single file converted example

---
+++ src/SingleFileAppConverted/HeyThere.cs
@@ -0,0 +1,7 @@
+﻿
+using Spectre.Console;
+
+var calendar = new Calendar(2025, 11);
+calendar.AddCalendarEvent(2025, 11, 16);
+calendar.HighlightStyle(Style.Parse("blue bold"));
+AnsiConsole.Write(calendar);
